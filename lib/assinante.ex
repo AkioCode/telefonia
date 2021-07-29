8aff3ba448c5b742916a3b5e16ce6b9b72be12d4
Rodrigo Otsuka <akio2631@gmail.com>
2021-07-29 08:25:46 +0000

    🎨 Simplify functions

--- lib/assinante.ex
+++ lib/assinante.ex
@@ -7,22 +7,28 @@ defmodule Assinante do
     pos_pago: "pos.txt"
   }
 
-  defp buscar(numero, plano) do
-    ler("pre.txt") ++ ler("pos.txt")
-    |> Enum.find_value(false, fn assinante -> assinante.numero == numero end)
-  end
+  def assinantes_pre, do: ler("pre.txt")
+  def assinantes_pos, do: ler("pos.txt")
+
+  def assinantes, do: ler("pre.txt") ++ ler("pos.txt")
+
+  defp filtro(lista, numero), do: Enum.find(lista, false, &(&1.numero == numero))
+
+  def buscar(numero, :pre_pago), do: filtro(assinantes_pre(), numero)
+  def buscar(numero, :pos_pago), do: filtro(assinantes_pos(), numero)
+  def buscar(numero), do: filtro(assinantes(), numero)
 
   def cadastrar(nome, numero, cpf, plano \\ :pre_pago) do
-    case buscar(numero, plano) do
-      true ->
-        {:error, "Já existe assinante com esse número"}
-
+    case buscar(numero) do
       false ->
         [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano} | ler(@assinantes[plano]) ]
         |> :erlang.term_to_binary()
         |> escrever(@assinantes[plano])
 
         {:ok, "Assinante (#{numero} - #{Atom.to_string(plano)}) cadastrado com sucesso"}
+
+      _assinante ->
+        {:error, "Já existe assinante com este número"}
     end
   end
 
