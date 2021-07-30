820e1cda87dc4672a06dbf292f15e259152a9572
Rodrigo Otsuka <akio2631@gmail.com>
2021-07-30 10:49:29 +0000

    📝 Add initial docs to Telefonia module

--- lib/telefonia.ex
+++ lib/telefonia.ex
@@ -1,10 +1,14 @@
 defmodule Telefonia do
-  @moduledoc false
+  @moduledoc """
+  Módulo de telefonia.
+  """
   def iniciar do
     File.write!("pre.txt", :erlang.term_to_binary([]))
     File.write!("pos.txt", :erlang.term_to_binary([]))
   end
 
+  @spec cadastrar_assinante(binary, binary, binary, atom) ::
+          {:error, <<_::296>>} | {:ok, <<_::64, _::_*8>>}
   def cadastrar_assinante(nome, numero, cpf, plano) do
     Assinante.cadastrar(nome, numero, cpf, plano)
   end
